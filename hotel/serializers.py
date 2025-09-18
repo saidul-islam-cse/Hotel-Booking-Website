d6979cac79f99954dfd5caf4d5318388a48faabb
saidulvai <saidulvaicsebgctub@gmail.com>
2025-09-18 17:24:06 +0600

    added all serializers

--- hotel/serializers.py
+++ hotel/serializers.py
@@ -1,12 +1,44 @@
 from rest_framework import serializers
-from .models import CustomUser, Profile
+from .models import CustomUser, Profile, Hotel, HotelImage, Booking, Review, Transaction
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['email', 'first_name', 'last_name', 'password']
+        fields = ['first_name', 'last_name','email', 'password']
     
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['wallet_balance','phone_number',]
+        fields = ['phone_number']
+
+class HotelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hotel
+        fields = ['name', 'address', 'city', 'description', 'price_per_night']
+
+class HotelImageSerializer(serializers.ModelSerializer):
+    hotel = HotelSerializer(read_only=True)
+    hotel_id = serializers.PrimaryKeyRelatedField(
+        queryset=Hotel.objects.all(), write_only=True, source='hotel'
+    )
+
+    class Meta:
+        model = HotelImage
+        fields = ['id', 'hotel', 'hotel_id', 'image', 'alt_text']
+        read_only_fields = ['id', 'hotel']
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ['hotel', 'check_in', 'check_out']
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['hotel', 'rating', 'comment']
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+        read_only_fields = ['id', 'user', 'booking', 'amount', 'transaction_type', 'created_at']
